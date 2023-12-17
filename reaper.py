b30233bc0ca8fa0b548af601fd78d0fb8003f118
probonopd <probonopd@users.noreply.github.com>
2023-12-17 20:23:02 +0100

    Modify Dexed state and write RPP

--- reaper.py
+++ reaper.py
@@ -39,8 +39,42 @@ for begin_line_number in dexed_vst3i_line_numbers:
     print("Decoded blob 1 in hex: " + decoded_blob1.hex()) # Meaning unclear; does it contain the DX7 In and DX7 Out MIDI ports?
     print("Decoded blob 2:")
     dexed_state_dict = dexed.read_dexed_xml(decoded_blob2)
+    print("Original dexed_state_dict:")
     print(json.dumps(dexed_state_dict, indent=4))
+
+    # Edit dexed_state_dict here
+    # dexed_state_dict['dexedState']['@currentProgram'] = "30" # This does not actually change the loaded voice; we would probably need to change the ['dexedState']['dexedBlob']['@currentProgram']
+    dexed_state_dict['dexedState']['@opSwitch'] = "000001" # Only operator 1 is on
+    dexed_state_dict['dexedState']['@wheelMod'] = "7 1 1 1"
+    dexed_state_dict['dexedState']['@footMod'] = "8 1 1 1"
+    dexed_state_dict['dexedState']['@breathMod'] = "9 1 1 1"
+    dexed_state_dict['dexedState']['@aftertouchMod'] = "10 1 1 1"
+
+    # Encode dexed_state_dict back to XML
+    dexed_state_xml = dexed.construct_dexed_xml(dexed_state_dict)
+    # Replace dexed_state_xml inside decoded_blob2
+    # Inside decoded_blob2, replace everything between the first '<' and the last '>' (including those) with dexed_state_xml
+    decoded_blob2 = decoded_blob2[:decoded_blob2.find(b'<')] + dexed_state_xml.encode('ascii') + decoded_blob2[decoded_blob2.rfind(b'>')+1:]
+
+    # Just for checking, decode the modified decoded_blob2 and print it
+    print("Modified decoded blob 2:")
+    dexed_state_dict = dexed.read_dexed_xml(decoded_blob2)
+    print(json.dumps(dexed_state_dict, indent=4))
+
+    # Encode the modified decoded_blob2 back to base64
+    blob2 = base64.b64encode(decoded_blob2).decode('ascii')
+    # Split into lines of 128 characters
+    blob2_lines = [blob2[i:i+128] for i in range(0, len(blob2), 128)]
+    # At the beginning of each line, add indentation_level + 2 spaces
+    blob2_lines = [' ' * (intendantion_level + 2) + line for line in blob2_lines]
+
+    # Replace the lines for blob 2 with the modified blob 2 lines
+    lines[begin_line_number + 2:end_line_number-1] = blob2_lines
 
     # From blob 3, get the preset name by removing everything before the first null byte, and removing everything after the second null byte
     preset_name = decoded_blob3.decode('ascii').split('\x00')[1].split('\x00')[0]
     print("Preset name from blob 3: " + preset_name)
+
+# Write the modified project file
+with open(os.path.dirname(__file__) + '/reaper_modified.rpp', 'w') as f:
+    f.write('\n'.join(lines))
