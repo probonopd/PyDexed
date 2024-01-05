2cc922e69799c4bd06003cfb52884d890b413f96
probonopd <probonopd@users.noreply.github.com>
2024-01-05 19:34:44 +0000

    DX7II ACED: Modulations, unison

--- create-rpp.py
+++ create-rpp.py
@@ -3,9 +3,14 @@
 # pip install --upgrade attrs ply
 
 # Add the rppgit subdirectory to the path
-import sys, base64, math
+import sys, os, base64, math
 
-sys.path.append("rppgit") # Rename rpp git directory so it doesn't conflict with the rpp module
+# If directory "rppgit" does not exist, clone the "rpp" git repository
+# but rename rpp git directory "rppgit" so it doesn't conflict with the "rpp" module
+if not os.path.isdir("rppgit"):
+    os.system("git clone https://github.com/Perlence/rpp rppgit")
+
+sys.path.append("rppgit")
 
 # Load the rpp module from the rpp subdirectory; DO NOT load rpp from the current directory
 import rpp
@@ -30,6 +35,7 @@ for instance in all_vsts:
 assert len(dexed_instances) == 128
 
 # Find all "midi/midi_note_filter" JS tags
+# (used to set the lowest and highest note for each voice for split mode defined in DX7II performance PCED)
 all_jss = r.findall('.//JS') # Find all JS tags
 midi_note_filters = []
 for js in all_jss:
@@ -39,10 +45,18 @@ print(len(midi_note_filters))
 assert len(midi_note_filters) == 128
 
 # Find all "utility/volume_pan" JS tags
+# (used to set the stereo pan, and to set the volume reduction for dual and split modes defined in DX7II performance PCED)
 volume_pans = []
 for js in all_jss:
     if js.attrib[0] == "utility/volume_pan":
         volume_pans.append(js)
+
+# Find all "IX/MIDI_Router" JS tags
+# (used to set the additional note shift = additional transpose defined in DX7II performance PCED)
+midi_routers = []
+for js in all_jss:
+    if js.attrib[0] == "IX/MIDI_Router":
+        midi_routers.append(js)
 
 import reaper
 import dexed
@@ -85,7 +99,6 @@ def set_dexed_state(instance, DexedState):
     new_children.append(instance.children[-1])
     instance.children = new_children
 
-
 def get_sysex_program_number(i):
     # i can be 0 to 127
     # Get the sysex, the program, and the number of the voice in the bank
@@ -118,9 +131,38 @@ def get_sysex_program_number(i):
 
     return sysex, vced, voice_number
 
+# AMEM, which can be converted to ACED, is contained in 'DX7IIfd ROM1A.zip' from
+# https://github.com/asb2m10/dexed/issues/165#issuecomment-1436586010
+# There are 32 performances x 35 bytes AMEM data = 1120 bytes AMEM data in each of the 2 syx files
+# starting at offset 16178 (0x3F82) according to
+# https://github.com/BobanSpasic/MDX_PerfConv/issues/5#issuecomment-1807177759
+full_dumps = ["DX7IIfd ROM1A 1-32.syx", "DX7IIfd ROM1A 33-64.syx"]
+# Extract the AMEM data from the syx files
+amems = []
+for full_dump in full_dumps:
+    f = open(full_dump, "rb")
+    dump = f.read()
+    f.close()
+    # Extract the AMEM data from the dump
+    for i in range(32):
+        amem = dump[16178 + i*35:16178 + i*35 + 35]
+        amems.append(amem)
+print("Number of AMEMs:", len(amems))
+# Convert the AMEM data to ACED data
+aceds = []
+for amem in amems:
+    aced = dx7II.amem2aced(amem)
+    a = dx7II.aced()
+    a.set_bytes(aced)
+    aceds.append(a)
+
+# TODO: While we're at it, we might also extract the VCEDs and PCEDs from the syx files in the same way
+# instead of using other syx files for them below
+
 i = 0
 j = 0
 n = 0
+performance_names = []
 voice_names = []
 detuned_performance_names = []
 
@@ -129,7 +171,6 @@ for instance in dexed_instances:
 
     n_is_uneven = n%2 == 1
 
-
     # Read a PCED from a file
     if n < 64:
         performance_filename = "DX7IIFDPerf.SYX"
@@ -137,15 +178,10 @@ for instance in dexed_instances:
         performance_filename = "DX7IIFDPerfB.SYX"
     PS = dx7II.PerformanceSyx(performance_filename)
 
-    print("Performance:", PS.pceds[j].pnam)
+    performance_name = PS.pceds[j].pnam.strip()
+    print("Performance:", performance_name)
+    
     print("i:", i, "j:", j, "n:", n)
-    voice_nr = 0
-    if n_is_uneven:
-        print("Voice B:", PS.pceds[j].vnmb)
-        voice_nr = PS.pceds[j].vnmb
-    else:
-        print("Voice A:", PS.pceds[j].vnma)
-        voice_nr = PS.pceds[j].vnma
 
     split_point = PS.pceds[j].sppt
     print("Split point:", split_point)
@@ -155,54 +191,115 @@ for instance in dexed_instances:
     reduction = 20 * math.log10(1 / math.sqrt(number_of_voices))
     reduction = round(reduction, 1)
 
+    is_unison = False
+
     if PS.pceds[j].plmd == 0:
-        print("Mode: Poly")
-        midi_note_filters[n].children[0][0] = "0"
-        midi_note_filters[n].children[0][1] = "127"
-        volume_pans[n].children[0][0] = str(reduction)
+        print("Mode: Single")
+        midi_note_filters[n].children[0][0] = "0" # Lowest sounding note
+        midi_note_filters[n].children[0][1] = "127" # Highest sounding note
+        # Check if unison is enabled in the ACED; pmod bit1: unison on/off
+        if aceds[j].pmod & 0b10 == 0b10:
+            is_unison = True
+            performance_name += " (Single, Unison)"
+            volume_pans[n].children[0][0] = str(reduction) # Reduce volume because two voices are active at the same time
+        else:
+            performance_name += " (Single)"
+            volume_pans[n].children[0][0] = "-0.0" # No reduction since only one voice is active at a time
+            volume_pans[n].children[0][1] = "50" # Center pan since only one voice is active at a time
+            if n_is_uneven:
+                # Set voice B to be silent
+                midi_note_filters[n].children[0][0] = "0" # Lowest sounding note
+                midi_note_filters[n].children[0][1] = "0" # Highest sounding note
+
     elif PS.pceds[j].plmd == 1:
         print("Mode: Dual")
-        midi_note_filters[n].children[0][0] = "0"
-        midi_note_filters[n].children[0][1] = "127"
-        volume_pans[n].children[0][0] = str(reduction)
+        performance_name += " (Dual)"
+        midi_note_filters[n].children[0][0] = "0" # Lowest sounding note
+        midi_note_filters[n].children[0][1] = "127" # Highest sounding note
+        volume_pans[n].children[0][0] = str(reduction) # Reduce volume because two voices are active at the same time
     elif PS.pceds[j].plmd == 2:
         print("Mode: Split")
+        performance_name += " (Split)"
         if n_is_uneven:
-            midi_note_filters[n].children[0][0] = str(split_point)
-            midi_note_filters[n].children[0][1] = "127"
+            # Voice B
+            midi_note_filters[n].children[0][0] = str(split_point) # Lowest sounding note
+            midi_note_filters[n].children[0][1] = "127" # Highest sounding note
         else:
-            midi_note_filters[n].children[0][0] = "0"
-            midi_note_filters[n].children[0][1] = str(split_point -1)
+            # Voice A
+            midi_note_filters[n].children[0][0] = "0" # Lowest sounding note
+            midi_note_filters[n].children[0][1] = str(split_point -1) # Highest sounding note
         volume_pans[n].children[0][0] = "-0.0" # No reduction since only one voice is active at a time due to the split point
 
-    ###
-    # TODO: Figure out how this stuff works
+    # Volume balance (Note: DX7II Pan Modes are currently not supported)
     volume_balance = PS.pceds[j].blnc
-    print("Volume balance:", volume_balance)
-    pan_mode = PS.pceds[j].pnmd
-    print("Pan mode:", pan_mode)
-    pan_control_range = PS.pceds[j].panrng
-    print("Pan control range:", pan_control_range)
-    ###
+    if volume_balance != 50:
+        performance_name = performance_name.replace(")", ", Volume Balance " + str(volume_balance) + ")")
+        print("Volume balance:", volume_balance)
+        # If volume balance is 50, then the volume of voice A and B is unchanged.
+        # Educated guess (to be verified):
+        # If the volume balance is 0, then voice B is silent and voice A is at full volume.
+        # If the volume balance is 100, then voice A is silent and voice B is at full volume.
+        # If the volume balance is 25, then voice A is at 75% volume and voice B is at 25% volume.
+        # volume_pans[n].children[0][0] expects a value in decibels, so we need to convert the volume balance to decibels like so:
+        old_volume_adjustment = float(volume_pans[n].children[0][0]) # Decibels
+        if n_is_uneven:
+            # Voice B
+            new_volume_adjustment = old_volume_adjustment + (volume_balance - 50) / 2
+        else:
+            # Voice A
+            new_volume_adjustment = old_volume_adjustment + (50 - volume_balance) / 2
+        volume_pans[n].children[0][0] = str(new_volume_adjustment)
 
     # Detune
+    unsion_detune = aceds[j].udtn
     dual_detune = PS.pceds[j].ddtn
-    if dual_detune != 0:
-        detuned_performance_names.append(PS.pceds[j].pnam)
-    print("Dual detune:", dual_detune)
+    total_detune = unsion_detune + dual_detune
+    if total_detune != 0:
+        performance_name = performance_name.replace(")", ", Detuned)")
+        if n_is_uneven:
+            detuned_performance_names.append(performance_name)
+    print("Total detune:", total_detune)
     # Convert detune value to what Dexed masterTune expects: -1398101 .. 1398101 # TODO: Check whether this is correct; why 1398101?
-    # This amount is likely far too much. We need to find out how Dexed converts the detune value to cents.
-    converted_detune = int(dual_detune) / 7 * 1398101
+    # According to the DX7II manual, when dual detune is 0, the detune is 0 cents. When it is 7, the detune is 1/4 step (50 cents) up and 1/4 step (50 cents) down
+    # 1 semitone = 1/2 step = 100 cents. So 1/4 step = 50 cents
+    # The maximum detune in Dexed is 1 semitone up and 1 semitone down, so 100 cents up and 100 cents down. (-1398101 .. 1398101)
+    converted_detune = int(total_detune) / 7 * 1398101 / 2
     if n_is_uneven:
         DS.dexed_state_dict['dexedState']['@masterTune'] = str(converted_detune)
     else:
         DS.dexed_state_dict['dexedState']['@masterTune'] = "-" + str(converted_detune)
 
-    print(midi_note_filters[n].children[0])
-    print(volume_pans[n].children[0])
+    voice_nr = 0
 
+    if not is_unison:
+        if n_is_uneven:
+            print("Voice B:", PS.pceds[j].vnmb)
+            voice_nr = PS.pceds[j].vnmb
+        else:
+            print("Voice A:", PS.pceds[j].vnma)
+            voice_nr = PS.pceds[j].vnma
+    else:
+        print ("Unison:", PS.pceds[j].vnma)
+        voice_nr = PS.pceds[j].vnma
+            
     sysex, vced, voice_number = get_sysex_program_number(voice_nr)
     voice_name = dx7.get_voice_name(vced)
+
+    # Note shift
+    # 24 means no shift. 12 means 1 octave down, 36 means 1 octave up; 0 means 2 octaves down, 48 means 2 octaves up
+    # Dexed has 0 as no shift. So we need to convert the note shift value from the DX7II to the Dexed value by subtracting 24
+    if n_is_uneven:
+        note_shift = PS.pceds[j].nsftb
+    else:
+        note_shift = PS.pceds[j].nsfta
+    if note_shift != 24:
+        converted_note_shift = note_shift - 24
+        voice_name += " (Note Shift " + str(converted_note_shift) + ")"
+        print("Additional note shift:", converted_note_shift)
+        midi_routers[n].children[0][5] = str(converted_note_shift) # Additional note shift (in addition to what the voice data already specifies)
+    else:
+        midi_routers[n].children[0][5] = "0" # No additional note shift
+
     print("Voice name:", voice_name)
     voice_names.append(voice_name)
 
@@ -216,6 +313,35 @@ for instance in dexed_instances:
     # Set the currently selected voice
     DS.dexed_state_dict['dexedState']['@currentProgram'] = str(voice_number)
 
+    # Set mod wheel, foot controller, breath controller, and aftertouch
+    # TODO: Check whether this logic is correct (most likely not)
+    # Dexed has a matrix for Wheel, Foot, Breath, Aftertouch x Pitch, Amp, EG Bias = 12 fields that can be set to 0 or 1,
+    # prepended by the amount. But the DX7II ACED allows to set different amounts for pitch, amp, and eg bias.
+    # So we need to convert the DX7II values to a single amount for each source; for now, let's use the maximum among the 3 sources.
+    wheel_amount = max(aceds[j].mwpm, aceds[j].mwam, aceds[j].mweb)
+    wheel_pitch_on = 1 if aceds[j].mwpm > 0 else 0
+    wheel_amp_on = 1 if aceds[j].mwam > 0 else 0
+    wheel_eg_bias_on = 1 if aceds[j].mweb > 0 else 0
+    DS.dexed_state_dict['dexedState']['@wheelMod'] = "%s %s %s %s" % (wheel_amount, wheel_pitch_on, wheel_amp_on, wheel_eg_bias_on)
+
+    foot_amount = max(aceds[j].fc1pm, aceds[j].fc1am, aceds[j].fc1eb, aceds[j].fc1vl)
+    foot_pitch_on = 1 if aceds[j].fc1pm > 0 else 0
+    foot_amp_on = 1 if aceds[j].fc1am > 0 else 0
+    foot_eg_bias_on = 1 if aceds[j].fc1eb > 0 else 0
+    DS.dexed_state_dict['dexedState']['@footMod'] = "%s %s %s %s" % (foot_amount, foot_pitch_on, foot_amp_on, foot_eg_bias_on)
+
+    breath_amount = max(aceds[j].bcpm, aceds[j].bcam, aceds[j].bceb, aceds[j].bcpb)
+    breath_pitch_on = 1 if aceds[j].bcpm > 0 else 0
+    breath_amp_on = 1 if aceds[j].bcam > 0 else 0
+    breath_eg_bias_on = 1 if aceds[j].bceb > 0 else 0
+    DS.dexed_state_dict['dexedState']['@breathMod'] = "%s %s %s %s" % (breath_amount, breath_pitch_on, breath_amp_on, breath_eg_bias_on)
+
+    # TODO: Set Pitch Bend Range and Pitch Bend Step
+    # It seems that here, too, DX7II ACED allows to set more than one value for pitch bend range and pitch bend step depending on the source.
+
+    if n_is_uneven:
+        performance_names.append(performance_name)
+
     set_dexed_state(instance, DS)
     print("")
     
@@ -228,13 +354,6 @@ for instance in dexed_instances:
     if j == 32:
         j = 0
 
-# From both performance files, get the performance names
-performance_names = []
-performance_filenames = ["DX7IIFDPerf.SYX", "DX7IIFDPerfB.SYX"]
-for performance_filename in performance_filenames:
-    PS = dx7II.PerformanceSyx(performance_filename)
-    for i in range(32):
-        performance_names.append(PS.pceds[i].pnam)
 assert len(performance_names) == 64
 assert len(voice_names) == 128
 
--- dx7II.py
+++ dx7II.py
@@ -1,11 +1,141 @@
 # http://yates.ca/dx7/
 
+
+class aced:
+    """
+    A ACED (Additional Parameters) object contains
+    the additional DX7II parameters (in addition to the DX7 PCED voice data).
+    Documented on page Add-9 of the DX7II manual.
+    ACED is 74 bytes long but only 49 bytes are used, so we are only using 49 bytes.
+    """
+    def __init__(self):
+        self.scm6 = 0 # 0 .. 1 # OP6 scaling mode normal/fraction
+        self.scm5 = 0
+        self.scm4 = 0
+        self.scm3 = 0
+        self.scm2 = 0
+        self.scm1 = 0
+        self.ams6 = 0 # 0 .. 7 # OP6 amplitude modulation sensitivity
+        self.ams5 = 0
+        self.ams4 = 0
+        self.ams3 = 0
+        self.ams2 = 0
+        self.ams1 = 0
+        self.pegr = 0 # 0 .. 3 # Pitch EG range 8va/4va/1va/1/2va
+        self.ltgr = 0 # 0 .. 1 # LFO key trigger (delay) single/multi
+        self.vpsw = 0 # 0 .. 1 # Pitch EG by velocity switch on/off
+        self.pmod = 0 # 0 .. 3 # bit0: poly/mono, bit1: unison on/off
+        self.pbr = 2 # 0 .. 12 # Pitch bend range
+        self.pbs = 0 # 0 .. 12 # Breath bend step
+        self.pbm = 0 # 0 .. 2 # Breath bend mode low/high/k. on
+        self.rndp = 0 # 0 .. 7 # Random pitch fluctuation off/5c-41c
+        self.porm = 0 # 0 .. 1 # Portamento mode ftn/fllw fngrd/flltm
+        self.pqnt = 0 # 0 .. 12 # Portamento step
+        self.pos = 0 # 0 .. 99 # Portamento time
+        self.mwpm = 0 # 0 .. 99 # Modulation wheel pitch mod range
+        self.mwam = 0 # 0 .. 99 # Modulation wheel amplitude mod range
+        self.mweb = 0 # 0 .. 99 # Modulation wheel EG bias range
+        self.fc1pm = 0 # 0 .. 99 # Foot controller 1 pitch mod range
+        self.fc1am = 0 # 0 .. 99 # Foot controller 1 amplitude mod range
+        self.fc1eb = 0 # 0 .. 99 # Foot controller 1 EG bias range
+        self.fc1vl = 0 # 0 .. 99 # Foot controller 1 volume range
+        self.bcpm = 0 # 0 .. 99 # Breath controller pitch mod range
+        self.bcam = 0 # 0 .. 99 # Breath controller amplitude mod range
+        self.bceb = 0 # 0 .. 99 # Breath controller EG bias range
+        self.bcpb = 0 # 0 .. 100 # Breath controller pitch bias range
+        self.atpm = 0 # 0 .. 99 # Aftertouch pitch mod range
+        self.atam = 0 # 0 .. 99 # Aftertouch amplitude mod range (documentation says "ATPM"; probably a typo)
+        self.ateb = 0 # 0 .. 99 # Aftertouch EG bias range
+        self.atpb = 0 # 0 .. 100 # Aftertouch pitch bias range
+        self.pgrs = 0 # 0 .. 7 # Pitch EG rate scaling depth
+        # Bits 39-63 are reserved (null bytes)
+        # self.reserved = "\x00" * 25 # 25 null bytes
+        self.fc2pm = 0 # 0 .. 99 # Pitch mod range
+        self.fc2am = 0 # 0 .. 99 # Amplitude mod range
+        self.fc2eb = 0 # 0 .. 99 # EG bias range
+        self.fc2vl = 0 # 0 .. 99 # Volume range
+        self.mcpm = 0 # 0 .. 99 # Pitch mod range
+        self.mcam = 0 # 0 .. 99 # Amplitude mod range
+        self.mceb = 0 # 0 .. 99 # EG bias range
+        self.mcvl = 0 # 0 .. 99 # Volume range
+        self.udtn = 0 # 0 .. 7 # Unison detune depth
+        self.fccs1 = 0 # 0 .. 99 # Foot controller 1 use as CS1 switch off/on: 0/1
+
+    def get_bytes(self):
+        # 49 bytes
+        bytes = []
+        for field in [self.scm6, self.scm5, self.scm4, self.scm3, self.scm2, self.scm1, self.ams6, self.ams5, self.ams4, self.ams3, \
+                      self.ams2, self.ams1, self.pegr, self.ltgr, self.vpsw, self.pmod, self.pbr, self.pbs, self.pbm, self.rndp, \
+                          self.porm, self.pqnt, self.pos, self.mwpm, self.mwam, self.mweb, self.fc1pm, self.fc1am, self.fc1eb, \
+                              self.fc1vl, self.bcpm, self.bcam, self.bceb, self.bcpb, self.atpm, self.atam, self.ateb, self.atpb, \
+                                  self.pgrs, self.fc2pm, self.fc2am, self.fc2eb, self.fc2vl, self.mcpm, self.mcam, \
+                                      self.mceb, self.mcvl, self.udtn, self.fccs1]:
+            bytes.append(field)
+        assert len(bytes) == 49
+
+        return bytes
+
+    def set_bytes(self, bytes):
+        assert len(bytes) == 49
+
+        self.scm6 = bytes[0]
+        self.scm5 = bytes[1]
+        self.scm4 = bytes[2]
+        self.scm3 = bytes[3]
+        self.scm2 = bytes[4]
+        self.scm1 = bytes[5]
+        self.ams6 = bytes[6]
+        self.ams5 = bytes[7]
+        self.ams4 = bytes[8]
+        self.ams3 = bytes[9]
+        self.ams2 = bytes[10]
+        self.ams1 = bytes[11]
+        self.pegr = bytes[12]
+        self.ltgr = bytes[13]
+        self.vpsw = bytes[14]
+        self.pmod = bytes[15]
+        self.pbr = bytes[16]
+        self.pbs = bytes[17]
+        self.pbm = bytes[18]
+        self.rndp = bytes[19]
+        self.porm = bytes[20]
+        self.pqnt = bytes[21]
+        self.pos = bytes[22]
+        self.mwpm = bytes[23]
+        self.mwam = bytes[24]
+        self.mweb = bytes[25]
+        self.fc1pm = bytes[26]
+        self.fc1am = bytes[27]
+        self.fc1eb = bytes[28]
+        self.fc1vl = bytes[29]
+        self.bcpm = bytes[30]
+        self.bcam = bytes[31]
+        self.bceb = bytes[32]
+        self.bcpb = bytes[33]
+        self.atpm = bytes[34]
+        self.atam = bytes[35]
+        self.ateb = bytes[36]
+        self.atpb = bytes[37]
+        self.pgrs = bytes[38]
+        # self.reserved = bytes[39:64]
+        self.fc2pm = bytes[39]
+        self.fc2am = bytes[40]
+        self.fc2eb = bytes[41]
+        self.fc2vl = bytes[42]
+        self.mcpm = bytes[43]
+        self.mcam = bytes[44]
+        self.mceb = bytes[45]
+        self.mcvl = bytes[46]
+        self.udtn = bytes[47]
+        self.fccs1 = bytes[48]
+
+
 class pced:
 
     """
     A PCED (Performance Edit) object contains the data for a single performance edit.
     Documented on page Add-10 of the DX7II manual.
-    PCED = PMEM format is 51 bytes long?
+    PCED is 51 bytes long.
     """
     def __init__(self):
         self.plmd = 1 # 0/1/2: Single/Dual/Split
@@ -21,8 +151,8 @@ class pced:
         self.fsas = 1 # Foot Switch Assign 0: SUS, 1: POR, 2: KHLD, 3: SFT
         self.fsw = 3 # Foot Switch Bit 0: A, Bit 1: B, 0/1: Off/On
         self.sprng = 0 # 0 .. 7
-        self.nsfta = 24 # 0 .. 48
-        self.nsftb = 24 # 0 .. 48
+        self.nsfta = 24 # 0 .. 48 # Note Shift A
+        self.nsftb = 24 # 0 .. 48 # Note Shift B
         self.blnc = 0 # 0 .. 100 # Volume Balance (-50 .. +50)
         self.tvlm = 0 # 0 .. 99 # Total Volume
         self.csld1 = 0 # 0 .. 105 # Control Slider 1
@@ -109,6 +239,64 @@ class PerformanceSyx:
         # Populate self.pceds with the 32 PCEDs
         for i in range(32):
             self.pceds[i].set_bytes(bytes[i*51:(i+1)*51])
+
+def aced2amem(aced):
+    # Borrowed from https://github.com/rarepixel/dxconvert/blob/master/DXconvert/dx7.py
+    amem = [0]*35
+    amem[0] = aced[0] + (aced[1]<<1) + (aced[2]<<2) + (aced[3]<<3) + (aced[4]<<4) + (aced[5]<<5)
+    amem[1] = (aced[7]<<3) + aced[6]
+    amem[2] = (aced[9]<<3) + aced[8]
+    amem[3] = (aced[11]<<3) + aced[10]
+    amem[4] = aced[12] + (aced[13]<<2) + (aced[14]<<3) + (aced[19]<<4)
+    amem[5] = (aced[16]<<2) + aced[15]
+    amem[6] = (aced[18]<<4) + aced[17]
+    amem[7] = (aced[21]<<1) + aced[20]
+    for p in range(8, 25):
+        amem[p] = aced[p+14]
+    for p in range(26, 34):
+        amem[p] = aced[p+13]
+    amem[34] = (aced[48]<<3) + aced[47]
+    for i in range(len(amem)):
+        amem[i] = amem[i]&127
+    assert len(amem) == 35
+    return amem
+
+def amem2aced(amem):
+    # Borrowed from https://github.com/rarepixel/dxconvert/blob/master/DXconvert/dx7.py
+    aced = [0]*49
+    aced[0] = amem[0]&1
+    aced[1] = (amem[0]&2)>>1
+    aced[2] = (amem[0]&4)>>2
+    aced[3] = (amem[0]&8)>>3
+    aced[4] = (amem[0]&16)>>4
+    aced[5] = (amem[0]&32)>>5
+    aced[6] = amem[1]&0b111
+    aced[7] = (amem[1]&0b111000)>>3
+    aced[8] = amem[2]&0b111
+    aced[9] = (amem[2]&0b111000)>>3
+    aced[10] = amem[3]&0b111
+    aced[11] = (amem[3]&0b111000)>>3
+    aced[12] = amem[4]&0b11
+    aced[13] = (amem[4]&0b100)>>2
+    aced[14] = (amem[4]&0b1000)>>3
+    aced[19] = (amem[4]&0b1110000)>>4
+    aced[15] = amem[5]&0b11 
+    aced[16] = (amem[5]&0b111100)>>2
+    aced[17] = amem[6]&0b1111
+    aced[18] = (amem[6]&0b110000)>>4
+    aced[20] = amem[7]&1
+    aced[21] = (amem[7]&0b11110)>>1
+    for p in range(8, 24):
+        aced[p+14] = amem[p]
+    aced[38] = amem[24]&0b111
+    for p in range(26, 34):
+        aced[p+13] = amem[p]
+    aced[47] = amem[34]&0b111
+    aced[48] = (amem[34]&0b1000)>>3
+    for i in range(len(aced)):
+        aced[i] = aced[i]&127
+    assert len(aced) == 49 # NOTE: According to the DX7II manual, ACED is 74 bytes long. This routine skips the 25 reserved bytes from the DX7II manual
+    return aced
 
 def main():
     PCED = pced()
